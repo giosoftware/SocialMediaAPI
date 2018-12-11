3be95d0c8984e258408ffaaed7324e3ca2ba3d12
Sergio Martín <giosoftware@gmail.com>
2018-12-11 18:26:36 +0100

    Se han añadido las rutas addInterest y blockUser.

--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,16 @@
 'use strict';
 
 const express = require('express');
-const router = express.Router();
+const router = express.Router({ mergeParams: true } );
 
-const user = require("../controllers/user");
+const user = require('../controllers/user');
 
-router.post('/', user.create);
-
+//router.post('/', user.create); No tiene sentido que un usuario pueda crear otro
+router.put('/addInterest', user.addInterest);
+router.put('/blockUser', user.blockUser);
 router.get('/:id', user.read);
-
-router.put("/:id", user.update);
-
-router.delete("/:id", user.del);
+router.put('/:id', user.update);
+router.delete('/:id', user.del);
 
 
-module.exports = router
-
+module.exports = router;
