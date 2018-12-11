dd8d83754c774a5aeaf772379f1a9374d09cb06f
Sergio Martín <giosoftware@gmail.com>
2018-12-11 18:19:56 +0100

    Se ha añadido una clave más segura en SECRET_TOKEN

--- config/config.js
+++ config/config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     port: process.env.port || 3000,
-    db: process.env.MONGODB_URI || 'mongodb://localhost:27017/socialnetwork',
-    SECRET_TOKEN: 'miclavedetokens',
+    db: process.env.MONGODB_URI || 'mongodb://localhost:27017/socialmedia',
+    SECRET_TOKEN: 'iVURKmZG-D1iB3uszgMtRoNlUAcrPvmOZHeSDmI4b3ahcOrF9u7fFHdfmrRgmd4-GljqQsiWeV7K5UyRvQ2SJQ',
 }
