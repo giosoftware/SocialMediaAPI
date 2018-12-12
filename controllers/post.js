26e137d4b5baaaca1159d679fe5174bebe1acab7
Sergio Martín <giosoftware@gmail.com>
2018-12-12 08:41:14 +0100

    La función addLike se ha modificado para que utilice async/await.

--- controllers/post.js
+++ controllers/post.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Post = require('../models/post');
+const User = require('../models/user');
 
 function create(req, res) {
     const post = {
@@ -61,15 +62,19 @@ function update(req, res) {
         });
 }
 
-function addLike(req, res) {
-    Post.updateOne({ _id: req.body.postId }, { $inc: { l: 1 } })
-        .then(result => {
-            if (result == null) res.send('No se han encontrado registros');
-            else res.send(result);
-        })
-        .catch(err => {
-            res.send(err);
-        });
+async function addLike(req, res) {
+    try {
+        const user = await User.findOne({ _id: req.user });
+        if (!user) throw new Error('No se ha encontrado el usuario que ha realizado la acción.');
+        const post = await Post.updateOne(
+            { _id: req.body.postId },
+            { $inc: { l: 1 }, $push: { ln: user.prof.n } }
+        );
+        if (!post) throw new Error('No se ha encontrado la publicación.');
+        else res.send(post);
+    } catch (err) {
+        res.status(500).send({'message': `Ha habido un error al añadir el 'Me gusta': ${err.message}`});
+    }
 }
 
 module.exports = { create, read, del, update, addLike };
