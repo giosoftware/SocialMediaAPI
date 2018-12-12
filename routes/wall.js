33436919b7ccb0e407a8b69435b84d0765efcf38
Sergio Martín <giosoftware@gmail.com>
2018-12-12 08:42:18 +0100

    Se han quitado los espacios sobrantes.

--- routes/wall.js
+++ routes/wall.js
@@ -6,11 +6,8 @@ const router = express.Router();
 const wall = require("../controllers/wall");
 
 router.post('/', wall.create);
-
 router.get('/:id', wall.read);
-
 router.put("/:id", wall.update);
-
 router.delete("/:id", wall.del);
 
 module.exports = router
