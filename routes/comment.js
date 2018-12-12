ca6c206a995e7e8d02b286b34933688aca8c6940
Sergio Martín <giosoftware@gmail.com>
2018-12-12 08:42:00 +0100

    Se ha añadido la ruta para la función addLike.

--- routes/comment.js
+++ routes/comment.js
@@ -3,16 +3,12 @@
 const express = require('express');
 const router = express.Router();
 
-const comm = require("../controllers/comment");
+const comm = require('../controllers/comment');
 
+router.put('/addLike', comm.addLike);
 router.post('/', comm.create);
-
 router.get('/:id', comm.read);
+router.put('/:id', comm.update);
+router.delete('/:id', comm.del);
 
-router.put("/:id", comm.update);
-
-router.delete("/:id", comm.del);
-
-
-module.exports = router
-
+module.exports = router;
