1df012c3f0dce76f350f30fe9af2330c00394887
Sergio Martín <giosoftware@gmail.com>
2018-12-16 22:54:04 +0100

    Se han quitado las funciones para crear, editar y eliminar un muro, ya que no deberían estar a disposición del usuario

--- controllers/wall.js
+++ controllers/wall.js
@@ -1,24 +1,7 @@
 'use strict';
 
 const Wall = require('../models/wall');
-/*
-function create(req, res) {
-    const wall = {
-        uid: req.body.userId,
-        n: req.body.nickname,
-        m: req.body.month,
-        p: req.body.posts
-    };
 
-    Wall.create(wall)
-        .then(result => {
-            res.send(result);
-        })
-        .catch(err => {
-            res.send(err);
-        });
-};
-*/
 function read(req, res) {
     Wall.findOne({ _id: req.params.id })
         .then(result => {
@@ -28,35 +11,6 @@ function read(req, res) {
         .catch(err => {
             res.send(err);
         });
-};
-/*
-function del(req, res) {
-    Wall.deleteOne({ _id: req.params.id })
-        .then(result => {
-            if (result == null) res.send('No se han encontrado registros');
-            else res.send(result);
-        })
-        .catch(err => {
-            res.send(err);
-        });
-};
+}
 
-function update(req, res) {
-    const wall = {
-        uid: req.body.userId,
-        n: req.body.nickname,
-        m: req.body.month,
-        p: req.body.posts
-    };
-
-    Wall.updateOne({ _id: req.params.id }, wall)
-        .then(result => {
-            if (result == null) res.send('No se han encontrado registros');
-            else res.send(result);
-        })
-        .catch(err => {
-            res.send(err);
-        });
-};
-*/
 module.exports = { read };
