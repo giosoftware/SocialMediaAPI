25e5e8ce0be5ed433723bb78808a683696a57233
Sergio Martín <giosoftware@gmail.com>
2018-12-16 22:52:42 +0100

    Se ha modificado la función que elimina un usuario. Había un código que no paree muy util.

--- controllers/user.js
+++ controllers/user.js
@@ -16,8 +16,7 @@ function read(req, res) {
 function del(req, res) {
     User.deleteOne({ _id: req.params.id })
         .then(result => {
-            if (result == null) res.send('No se han encontrado registros');
-            else res.send(result);
+            res.json(result);
         })
         .catch(err => {
             res.send(err);
