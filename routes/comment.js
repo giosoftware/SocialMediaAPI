fc6e328cf0aeb0003d038edbb73d59ca9d670083
Sergio Martín <giosoftware@gmail.com>
2018-12-17 08:26:49 +0100

    Se ha añadido la ruta /removeLike.

--- routes/comment.js
+++ routes/comment.js
@@ -6,6 +6,7 @@ const router = express.Router();
 const comm = require('../controllers/comment');
 
 router.put('/addLike', comm.addLike);
+router.put('/removeLike', comm.removeLike);
 router.post('/', comm.createComment);
 router.get('/:id', comm.getComment);
 router.put('/:id', comm.updateComment);
